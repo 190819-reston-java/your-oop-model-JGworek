a096cddaceb515a2f15522061cc4665269b2347a
JGworek <jonathangworek@gmail.com>
2019-08-28 11:02:16 -0400

    there is now a pile of sandwiches

--- src/main/java/myOOP/SandwichApp.java
+++ src/main/java/myOOP/SandwichApp.java
@@ -1,5 +1,7 @@
 package myOOP;
 
+import java.util.ArrayList;
+
 public class SandwichApp {
 
 	public static void main(String[] args) {
@@ -7,6 +9,23 @@ public class SandwichApp {
 		Burger yum = new Burger("cheese" , "Burger with Cheese");
 		yum.heatUp(10);
 		yum.listIngredients();
+		Burger burger1 = new Burger("cheese", "The cheeseman");
+		TunaSanwich tuna1 = new TunaSanwich("cheese", "The cheesewoman");
+		Sandwich carlosDefault = new Burger();
+		
+		
+		ArrayList<Sandwich> pileOfSandwiches = new SandwichPile();
+		pileOfSandwiches.add(burger1);
+		pileOfSandwiches.add(tuna1);
+		pileOfSandwiches.add(carlosDefault);
+		
+		System.out.println("There are " + pileOfSandwiches.size() + " sandwiches in the pile");
+		if (pileOfSandwiches.size() > 5) {
+			System.out.println("That is too many sandwiches for any mere mortal.");
+		} else {
+			System.out.println("You should eat them all before they spoil.");
+		}
+		
 	}
 
 }
